b62a7645d2d446acb9047402ad0317b77bd39970
ifabregat <ifabregat@fi.uba.ar>
2024-07-14 01:01:09 -0300

    Add select and create/delete button to vinculate avion with linea

--- frontend/LineasAereas/Editar/scripts2.js
+++ frontend/LineasAereas/Editar/scripts2.js
@@ -1,5 +1,5 @@
 const params = new URLSearchParams(window.location.search);
-const id = params.get('id');
+const id_linea = params.get('id');
 
 function data_recibida(data) {
     return data.json();
@@ -15,7 +15,7 @@ function analizar_data(data) {
     document.getElementById('foto').value = data.linea.foto;
 }
 
-fetch (`http://localhost:5000/lineasaereas/${id}`)
+fetch (`http://localhost:5000/lineasaereas/${id_linea}`)
 .then(data_recibida)
 .then(analizar_data)
 .catch(analizar_error)
@@ -23,7 +23,7 @@ fetch (`http://localhost:5000/lineasaereas/${id}`)
 function respuesta_recibida (data) {
     if (data.mensaje === "Linea editada exitosamente") {
         alert("Linea editada exitosamente");
-        window.location.href = `/LineasAereas/LineaAerea/?id=${id}`;
+        window.location.href = `/LineasAereas/LineaAerea/?id=${id_linea}`;
     } else {
         alert("Error al editar la linea");
     }
@@ -38,7 +38,7 @@ function editar_linea(event){
     const codigo = formData.get('codigo');
     const foto = formData.get('foto');
 
-    fetch (`http://localhost:5000/lineasaereas/${id}`, {
+    fetch (`http://localhost:5000/lineasaereas/${id_linea}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
@@ -52,4 +52,51 @@ function editar_linea(event){
     .then((res) => res.json())
     .then(respuesta_recibida)
     .catch((error) => console.log("ERROR: ", error));
+}
+
+function analizar_data_aviones(data){
+    const container = document.getElementById('modelos');
+
+    for (let i = 0; i < data.aviones.length; i++) {
+        const avion = data.aviones[i];
+
+        const opcion = document.createElement('option');
+        opcion.setAttribute('value', avion.id);
+        opcion.innerText = `${avion.fabricante} ${avion.modelo}`;
+        container.appendChild(opcion);
+    }
+}
+
+fetch ("http://localhost:5000/aviones")
+.then(data_recibida)
+.then(analizar_data_aviones)
+.catch(analizar_error)
+
+function agregar_avion(){
+    const id_avion = document.getElementById('modelos');
+    const avion_id = `avion_${id_avion.value}`;
+
+    if (document.getElementById(avion_id)) {
+        alert("Ya se cargo ese avion");
+        return;
+    }
+
+    const li = document.createElement('li');
+    li.innerText = id_avion.options[id_avion.selectedIndex].text;
+    li.setAttribute("id", avion_id);
+
+    const borrar = document.createElement('button');
+    borrar.innerText = 'Borrar';
+    borrar.onclick = function(){ borrar_avion(avion_id) };
+    borrar.setAttribute("type", "button");
+    borrar.setAttribute("class", "btn btn-danger btn-sm ms-2");
+    li.appendChild(borrar);
+
+    const flota = document.getElementById('flota');
+    flota.appendChild(li);
+}
+
+
+function borrar_avion(id_avion){
+    document.getElementById(id_avion).remove();
 }
